2ccdaa9aabef25f1e0171115bb61737203600716
Khanh Vo <khanh.vo@tiki.vn>
2022-07-25 17:16:01 +0700

    Update Redis connection string

--- src/database/redis.ts
+++ src/database/redis.ts
@@ -1,8 +1,9 @@
 import log from "loglevel";
 import { createClient } from "redis";
 
-const { REDIS_PORT, REDIS_HOSTNAME } = process.env;
-const client = createClient({ socket: { host: REDIS_HOSTNAME, port: Number(REDIS_PORT) } });
+const { REDIS_PORT, REDIS_HOSTNAME, REDIS_PASS } = process.env;
+// const client = createClient({ socket: { host: REDIS_HOSTNAME, port: Number(REDIS_PORT) } });
+const client = createClient({ url: `redis://:${REDIS_PASS}@${REDIS_HOSTNAME}:${REDIS_PORT}` });
 
 client.on("error", (error) => {
   log.error(error);
