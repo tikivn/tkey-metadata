1c7be67b82d8e25ba918b8e5973907290c8efdeb
Khanh Vo <khanh.vo@tiki.vn>
2022-07-25 20:40:33 +0700

    Update redis connection string with password

--- src/database/redis.ts
+++ src/database/redis.ts
@@ -2,8 +2,8 @@ import log from "loglevel";
 import { createClient } from "redis";
 
 const { REDIS_PORT, REDIS_HOSTNAME, REDIS_PASS } = process.env;
-// const client = createClient({ socket: { host: REDIS_HOSTNAME, port: Number(REDIS_PORT) } });
-const client = createClient({ url: `redis://:${REDIS_PASS}@${REDIS_HOSTNAME}:${REDIS_PORT}` });
+const client = createClient({ socket: { host: REDIS_HOSTNAME, port: Number(REDIS_PORT) } });
+client.auth({ password: REDIS_PASS });
 
 client.on("error", (error) => {
   log.error(error);
